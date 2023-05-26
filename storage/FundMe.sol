b121f5e5ab00bae1ce533041a49ec8a9c3af3ce3
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-26 23:47:21 +0200

    Update FundMe.sol

--- storage/FundMe.sol
+++ storage/FundMe.sol
@@ -1,1 +1,42 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint;
+    uint public minimumUsd = 50 * 1e18;
+    mapping (address => uint) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    address[] public funders;
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimumUsd, "Not enough funds, aborting!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool isSent, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require (isSent, "The call function was unsuccesful");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+}
