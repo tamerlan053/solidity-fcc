71739d6877ceb9214ef5058e93f8785605113ae6
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-26 23:48:06 +0200

    Update PriceConverter.sol

--- storage/PriceConverter.sol
+++ storage/PriceConverter.sol
@@ -1,1 +1,25 @@
+//SPDX-License-Identifier: MIT 
 
+pragma solidity 0.8.15;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+        function getPrice() internal view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int price ,,,) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
+    }
+
+    function getVersion() internal view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);  
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint) {
+        uint ethPrice = getPrice();
+        uint ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
+        return ethAmountInUsd;
+    }
+}
