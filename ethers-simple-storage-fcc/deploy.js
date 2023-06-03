0dc4b373130d334f1d1a8c9a38fe29ca782eb471
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-06-03 23:41:19 +0200

    Update deploy.js

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -7,7 +7,7 @@ async function main() {
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-    console.log("Deploying, please wait...");
+    console.log("Deploying the contract, please wait...");
     const contract = await contractFactory.deploy();
     console.log(contract);
 }
