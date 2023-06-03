7610f21cad9c4f9babeef003a65b18643bb09893
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-06-03 23:40:09 +0200

    Update deploy.js

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -1,9 +1,15 @@
-const etherss = require("ethers")
+const ethers = require("ethers")
 const fs = require("fs-extra")
 
 async function main() {
-    const provider = new ethers.providers.JsonRpcProviders("http://127.0.0.1:7545")
-    const wallet = new ethers.Wallet("0xfc692474151fb5d5034d84a1cef267204cd04778b972b1a2ccfd9235e1e23c95", provider)
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:7545");
+    const wallet = new ethers.Wallet("0xfc692474151fb5d5034d84a1cef267204cd04778b972b1a2ccfd9235e1e23c95", provider);
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+    const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    console.log("Deploying, please wait...");
+    const contract = await contractFactory.deploy();
+    console.log(contract);
 }
 
 main()
