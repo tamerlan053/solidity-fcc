89e09ea1f406213503d131501c5ae5ff4293cfa9
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-06-03 22:14:55 +0200

    Update deploy.js

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -1,7 +1,9 @@
-const ethers = require("ethers")
+const etherss = require("ethers")
+const fs = require("fs-extra")
 
 async function main() {
-
+    const provider = new ethers.providers.JsonRpcProviders("http://127.0.0.1:7545")
+    const wallet = new ethers.Wallet("0xfc692474151fb5d5034d84a1cef267204cd04778b972b1a2ccfd9235e1e23c95", provider)
 }
 
 main()
